bcc5fb1358681271af037fcffb80fc7904c9fcf9
Maksim Zhbanov <gbanof96@gmail.com>
2021-05-02 16:25:42 +0300

    Изменил порт тестового стенда на 4000

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import app from "./config/app";
 
-const port = process.env.NODE_ENV == 'production' ? 3000 : 3001
+const port = process.env.NODE_ENV == 'production' ? 3000 : 4000
 
 app.listen(port, () => {
     console.log('Express server listening on port ' + port);
