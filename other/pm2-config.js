1b59d0c0cc5d866459af34554385a41903757cf3
Sokratik <gbanof96@gmail.com>
2023-01-04 23:09:08 +0300

    поправил конфиги для pm2

--- other/pm2-config.js
+++ other/pm2-config.js
@@ -4,14 +4,28 @@ module.exports = {
       name   : "prod",
       script : "./production/dist/index.js",
       env: {
-          NODE_ENV: "production"
+          NODE_ENV: "production",
+          APP_PRODUCTION_PORT: 3000,
+          APP_DEVELOPMENT_PORT: 4000,
+          APP_DISCORD_CLIENT_ID: 929926537422192741,
+          APP_DISCORD_CLIENT_SECRET: '1zSEsXy-xuRh3IzaKahhi5bmuU2bA0aM',
+          APP_DEVELOP_CLIENT_ROOT_PAGE: 'http://localhost:3000',
+          APP_PRODUCTION_CLIENT_ROOT_PAGE: 'http://45.141.101.120',
+          APP_SESSION_SECRET: 'xuRh3IzaKahhi5b'
       }
     },
     {
       name   : "test",
       script : "./test/dist/index.js",
       env: {
-        NODE_ENV: "development"
+        NODE_ENV: "development",
+        APP_PRODUCTION_PORT: 3000,
+        APP_DEVELOPMENT_PORT: 4000,
+        APP_DISCORD_CLIENT_ID: 929926537422192741,
+        APP_DISCORD_CLIENT_SECRET: '1zSEsXy-xuRh3IzaKahhi5bmuU2bA0aM',
+        APP_DEVELOP_CLIENT_ROOT_PAGE: 'http://localhost:3000',
+        APP_PRODUCTION_CLIENT_ROOT_PAGE: 'http://45.141.101.120:8080',
+        APP_SESSION_SECRET: 'xuRh3IzaKahhi5b'
       }
     }
   ]
