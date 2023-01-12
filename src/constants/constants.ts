ea6b27f4ffbdfd62c418fdbc660e213d36c6f739
Krygerik <36044698+Krygerik@users.noreply.github.com>
2023-01-12 23:42:23 +0300

    Поправил учетку для подключения к БД

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -7,4 +7,4 @@ const database = process.env.NODE_ENV == 'production' ? 'production' : 'test';
 /**
  * Урл до бд с учетом среды
  */
-export const mongoUrl = 'mongodb://AdminSokratik:Her0EsF!ve@localhost:27017/' + database;
+export const mongoUrl = 'mongodb://AdminSokratik:HEr0EsF!ve@localhost:27017/' + database;
