cd90996ae94aa27d932642b77ad5453e5435d289
Krygerik <36044698+Krygerik@users.noreply.github.com>
2023-04-23 19:00:42 +0300

    Добавил подключение сертификатов к продакшн сборке
    (cherry picked from commit a73070a0dd3aa5fa3b81b6865c06f76cfc5dbb3d)

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
+import { createServer } from 'https';
+import { readFileSync } from 'fs';
 import app from "./config/app";
 import {EEnvironment} from "./constants";
 
-if (process.env.NODE_ENV !== EEnvironment.UnitTests) {
-  app.listen(process.env.APP_PORT, () => {
-    console.log('Express server listening on port ' + process.env.APP_PORT);
-  })
-}
+const port = process.env.NODE_ENV == EEnvironment.Production && 3001
+    || process.env.NODE_ENV == EEnvironment.Test && 4001
+    || 4000;
 
-export default app;
+const server = process.env.NODE_ENV == 'production'
+    ? createServer(
+        {
+            key: readFileSync('/etc/letsencrypt/live/asha-heroes.ru/privkey.pem'),
+            cert: readFileSync('/etc/letsencrypt/live/asha-heroes.ru/fullchain.pem'),
+        },
+        app,
+    )
+    : app;
+
+if (process.env.NODE_ENV !== EEnvironment.UnitTests) {
+    server.listen(port, () => {
+        console.log('Express server listening on port ' + port);
+    })
+}
