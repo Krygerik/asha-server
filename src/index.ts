a73070a0dd3aa5fa3b81b6865c06f76cfc5dbb3d
Krygerik <36044698+Krygerik@users.noreply.github.com>
2023-04-23 19:00:42 +0300

    Добавил подключение сертификатов к продакшн сборке

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
+import { createServer } from 'https';
+import { readFileSync } from 'fs';
 import app from "./config/app";
 
 const port = process.env.NODE_ENV == 'production' ? 3001 : 4000
 
-app.listen(port, () => {
+const server = process.env.NODE_ENV == 'production'
+    ? createServer(
+        {
+            key: readFileSync('/etc/letsencrypt/live/asha-heroes.ru/privkey.pem'),
+            cert: readFileSync('/etc/letsencrypt/live/asha-heroes.ru/fullchain.pem'),
+        },
+        app,
+    )
+    : app;
+
+server.listen(port, () => {
     console.log('Express server listening on port ' + port);
 })
